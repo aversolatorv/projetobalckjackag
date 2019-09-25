f8423edf2da9e606c02ad3577b80e2332afbb331
gtkacz <55806524+gtkacz@users.noreply.github.com>
2019-09-25 20:30:34 -0300

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -6,35 +6,37 @@ k=10
 a=11
 
 baralho=[a, 2, 3, 4, 5, 6, 7, 8, 9, 10, q, j, k]*4
-
+mj=[] #mão jogador
+mc=[] #mão croupier
 d=1000
 
 m="carta"
 print("Bem-vindo à mesa de Blackjack!")
 while d!=0:
     print("Você tem R$ ", d)
-    a=int(input('Aposte um valor: '))
+    a=int(input('Aposte um valor inteiro: '))
     cj1=random.choice(baralho) #cj = carta do jogador
     cj2=random.choice(baralho)
-    if cj1+cj2>21:
+    mj.append(cj1, cj2)
+    if sum(mj)>21:
         a=1
-    if cj1+cj2>21:
+    if sum(mj)>21:
         print("Você estorou!")
         d=d-a
         continue
-    if cj1+cj2==21:
+    if sum(mj)==21:
         d=d+((1.5)*a)
         print("Parabéns, você ganhou um Blackjack ", "com as cartas", cj1, cj2, "!")
         continue
-    print("Você tem as cartas: ", cj1, "e", cj2, "totalizando: ", cj1+cj2, ", você gostaria de mais uma carta ou quer continuar?")
+    print("Você tem as cartas: ", mj[1], "e", mj[2], "totalizando: ", sum(mj), ", você gostaria de mais uma carta ou quer continuar?")
     while m!= "continuar":
         m=input("Responda com continuar ou carta: ")
-        cj3=random.choice(baralho)
-        if cj1+cj2>21:
+        mj.append(random.choice(baralho))
+        if sum(mj)>21:
             a=1
-        if cj1+cj2+cj3>21:
+        if sum(mj)>21:
             print("Você estorou!")
-        elif cj1+cj2+cj3==21:
+        elif sum(mj)==21:
             d=d+((1.5)*a)
             print("Parabéns, você ganhou um Blackjack!")
         break
