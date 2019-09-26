87aaf5c0262ccede1ceaad5602520f8d2b178b56
aversolatorv <55805198+aversolatorv@users.noreply.github.com>
2019-09-25 22:58:15 -0300

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,27 +1,29 @@
 import random
 
-q=10
+q=10 #figuradas valem 10
 j=10
 k=10
-a=11
-binicial=[a, 2, 3, 4, 5, 6, 7, 8, 9, 10, q, j, k]
-baralho=(binicial)*4
+a=11 #ás vale 11 se uma mão não estourar
+binicial=[a, 2, 3, 4, 5, 6, 7, 8, 9, 10, q, j, k]*4 #um baralho tem 4 conjuntos desses
+baralho=(binicial) #nao tinhamos certeza se podemos multiplicar uma lista ja multiplicada
 mj=[] #mão jogador
 mc=[] #mão croupier
-d=1000
+d=1000 #dinheiro
+m="carta" #para entrar no loop
 
-m="carta"
 print("♠♥♣♦ Bem-vindo à mesa de Blackjack! ♠♥♣♦")
 print("Regras e funcionamento:")
 print("1: Não aposte mais dinheiro do que tem!")
 print("2: Aposte valores inteiros!")
 print("3: Se suas cartas iniciais somarem 21 pontos, você automaticamente ganha 1,5x o que apostou.")
 print("4: Enquanto a mão do croupier for menor que a sua, ele vai puxar cartas até atingir no mínimo 17 pontos.")
-print("5: Se você ganhar/perder de outra forma você ganha/perde exatamente o que apostou.")
-print("6: Se quiser que o jogo pare, digite 'desisto'.")
+print("5: Para razões de desempate: ♣>♡>♠>♢") #Feature livre implementada por nós
+print("6: Se você ganhar/perder de outra forma você ganha/perde exatamente o que apostou.")
+print("7: Se quiser que o jogo pare, digite 'desisto'.")
+
 print("Se divirta!")
-q=0
-inp="s"
+q=0 #para entrar no loop
+inp="s" #para entrar no loop
 while q<1 or q>10 or inp=="Não" or inp=="não" or inp=="nao" or inp=="Nao" or inp=="n" or inp=="N":
     q=int(input("Com quantos baralhos quer jogar? "))
     if q>10:
@@ -37,6 +39,8 @@ while q<1 or q>10 or inp=="Não" or inp=="não" or inp=="nao" or inp=="Nao" or i
         continue
     elif inp=="Sim" or inp=="sim" or inp=="s" or inp=="S":
         break
+#Utilizamos varias versoes de 'Sim' e 'Nao' para facilitar para o usuario    
+
 baralho=baralho*q
 while d!=0:
     mj.clear()
@@ -59,7 +63,7 @@ while d!=0:
     if sum(mj)>21:
         a=1
     if sum(mj)>21:
-        print("Você estourou", "com as cartas", mj, "!")
+        print("Você estourou com as cartas", mj, "!")
         d=d-a
         continue
     if sum(mj)==21:
@@ -68,16 +72,16 @@ while d!=0:
         continue
     print("Você tem as cartas: ", mj, "totalizando: ", sum(mj), ", você gostaria de mais uma carta ou quer continuar?")
     m=input("Responda com 'continuar' ou 'carta': ")
-    if m=="continuar":
+    if m=="carta":
         while m!= "continuar":
             m=input("Responda com 'continuar' ou 'carta': ")
             mj.append(random.choice(baralho))
             if sum(mj)>21:
                 a=1
             if sum(mj)>21:
-                print("Você estourou", "com as cartas", mj, "!")
+                print("Você estourou com as cartas", mj, "totalizando:", sum(mj), "!")
                 d=d-a
-                continue
+                break
             elif sum(mj)==21:
                 d=d+((1.5)*a)
                 print("Parabéns, você ganhou um Blackjack!")
